953e8bb8053be85261635d5f195ba89e0875feee
Seedockh <pierre.herisse@gmail.com>
2020-02-11 10:57:54 +0100

    TD-2 Done

--- app/src/main/java/tasklist/TaskListFragment.kt
+++ app/src/main/java/tasklist/TaskListFragment.kt
@@ -8,9 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoherissepierre.R
 import kotlinx.android.synthetic.main.fragment_task_list.*
+import java.util.*
 
 class TaskListFragment : Fragment() {
-    private val taskList: List<Task> = listOf(
+    private val taskList: MutableList<Task> = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
@@ -28,5 +29,9 @@ class TaskListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         fragment_recycler_view.layoutManager = LinearLayoutManager(activity)
         fragment_recycler_view.adapter = TaskListAdapter(taskList)
+        add_task_button.setOnClickListener {
+            taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
+            fragment_recycler_view.adapter!!.notifyDataSetChanged()
+        }
     }
 }
